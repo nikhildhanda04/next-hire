9a9b6532ed520f3ccdb50d1feb89d06513e3fae3
Nikhil Dhanda <nikhildhanda84@gmail.com>
2025-07-31 18:54:58 +0530

    minor chnages

--- src/app/components/landingpage/whyus.tsx
+++ src/app/components/landingpage/whyus.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Whyus() {
     return(
         <>
-        <div className="flex flex-col gap-12 px-8 md:px-24 lg:px-52 -mt-88 md:mt-0">
-            <div className="font-primary text-5xl pl-16 md:pl-4 text-dark dark:text-light ">
+        <div className="flex flex-col gap-12 px-4 md:px-24 lg:px-52 -mt-80 md:mt-0">
+            <div className="font-primary text-5xl pl-12 md:pl-4 text-dark dark:text-light ">
                 Why Us?
             </div>
 
