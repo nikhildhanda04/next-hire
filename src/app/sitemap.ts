3e3f7e2ecf9492362a1474c3be4afd615aa80b9f
nikhildhanda04 <nikhildhanda84@gmail.com>
2025-12-05 21:09:38 +0530

    added: robots and sitemap and seo

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl = 'https://next-hire-bice.vercel.app';
+
+    return [
+        {
+            url: baseUrl,
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/dashboard`,
+            lastModified: new Date(),
+            changeFrequency: 'daily',
+            priority: 0.8,
+        },
+        // Add other static routes as needed
+    ];
+}
